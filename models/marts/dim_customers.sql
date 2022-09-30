724f8e34ab2b87ead00f9e3e1889c0144e37e2e4
Angelica Lastra <angelica.lastra@fishtownanalytics.com>
2022-09-30 18:15:15 +0000

    structure/style changes to dim_customer

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -6,41 +6,49 @@
     )
 }}
 
-with customer as (
+with
+
+customer as (
 
     select * from {{ ref('stg_tpch_customers') }}
 
 ),
+
 nation as (
 
     select * from {{ ref('stg_tpch_nations') }}
 ),
+
 region as (
 
     select * from {{ ref('stg_tpch_regions') }}
 
 ),
+
 final as (
+
     select 
-        customer_id,
+
+        customer.customer_id,
         customer.name,
-        address,
-        -- nation.nation_id as nation_id,
+        customer.address,
+
+        nation.nation_id,
         nation.name as nation,
-        {# region.region_id as region_id, #}
+        
+        region.region_id as region_id,
         region.name as region,
-        phone_number,
-        account_balance,
-        market_segment
+        region.phone_number,
+        region.account_balance,
+        region.market_segment
+
     from
         customer
         inner join nation
             on customer.nation_id = nation.nation_id
-        inner join region on nation.region_id = region.region_id
+        inner join region
+            on nation.region_id = region.region_id
+    order by 1
 )
-select 
-    *
-from
-    final
-order by
-    customer_id
+
+select * from final
